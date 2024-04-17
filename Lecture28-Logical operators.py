5993e6c7b0d614fc82d8fbb833aabecc40103df0
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 01:18:09 -0500

    Lecture28-Logical operators
    True or False

---
+++ Lecture28-Logical operators.py
@@ -0,0 +1,15 @@
+'''
+x = True
+y = False
+z = x or y
+print(z)
+'''
+'''
+a = 10
+b = 5
+c = a > b
+print(c)
+'''
+age = 17
+print("Hi there. Are you older than 18?")
+print(age>18) #age and 18 are data, > greater than is an operator to show that data is True or False.
