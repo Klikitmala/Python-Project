a39ce1714b43b8d60d2481ad98909f26dc280d5f
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 01:20:38 -0500

    Lecture29-Assignment Operator
    a = 5
    a = a+5 (a+=5)
    a = a-1 ( a-=1)
    print(a) =>9

---
+++ Lecture29-Assignment Op.py
@@ -0,0 +1,18 @@
+'''
+a = 10
+b = 5
+c = a + b
+b = 10
+print(b) #answer is 10 because b data was changed to 10.
+'''
+'''
+a = 10 
+b = 12
+c = a + b
+a += 5
+print(a)
+'''
+a = 10
+a = a + 5
+a = a - 1
+print(a)
