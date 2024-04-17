12878ce05f19afe5c06cd8c52ca97d6f42c6c7b6
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 01:22:13 -0500

    Lecture32-Input data
    Input data by keyboard's users.

---
+++ Lecture32-Input data.py
@@ -0,0 +1,18 @@
+#x = input()
+#print("Hello",x)
+'''
+x = input()
+print(type(x))
+'''
+#name = input()
+#lastName = input()
+#print("Hello",name,lastName)
+
+'''
+name = input("What's your firstname?")
+lastName = input("What's your lastname?")
+print("Hello",name,lastName)
+'''
+x = int(input("x ="))
+y = int(input("y ="))
+print("Answer =",(x+y))
