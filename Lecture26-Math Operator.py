00eab1eb358c2287eda3b5229b726565b62208a8
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 01:17:15 -0500

    Lecture26-Math Operator
    + - * /

---
+++ Lecture26-Math Operator.py
@@ -0,0 +1,14 @@
+'''
+x = 10
+y = 5
+z = x+y
+print(z)
+'''
+'''
+money = 150
+incomePerDay = 300
+costPerDay = 120
+result = 150+(300*30)-(120*30) #money left per month for saving
+print(result)
+'''
+print(10+5*2*8/2+4)
