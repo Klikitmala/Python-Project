208a02395062a261fdcbf3beb3c8d019dae74789
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 12:35:53 -0500

    Lecture36_if else Condition
    if else condition.

---
+++ Lecture36_if esle condition.py
@@ -0,0 +1,28 @@
+if True:
+    print("Hello Condition1") #It is under "if con".
+    print("Hello Condition2") #It is under "if con".
+print("-----------------------------------------")    
+if False:
+    print("Hello Condition1") #It is under "if con".
+    print("Hello Condition2") #It is under "if con".
+print("-----------------------------------------")
+if False:
+    print("Hello Condition1")
+print("Hello Condition2")#It isn't under if condition.So it shows the result.
+print("-------------------------------------------")
+
+#This Condition is False, so it goes to show messege under else block.
+money = 100
+if money > 300 :
+    print("Go to KFC!!!")
+else :
+    print("So SAD!!! T_T")
+print("Bye Bye")
+print("------------------------------")
+#This Condition is True, so works under if block.
+money = 1000
+if money > 300 :
+    print("Go to KFC!!!")
+else :
+    print("So SAD!!! T_T")
+print("Bye Bye")
